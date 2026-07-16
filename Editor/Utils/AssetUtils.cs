b13e913ea85e0bbdd54fe5dd39b556785827d1d2
Haï~ <hai-vr@users.noreply.github.com>
2026-07-16 12:46:13 +0200

    refactor: alternative for local package folder

--- Editor/Utils/AssetUtils.cs
+++ Editor/Utils/AssetUtils.cs
@@ -1,7 +1,7 @@
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using UnityEditor.PackageManager;
 
 namespace jp.lilxyzw.shadercore
 {
@@ -10,24 +10,13 @@ namespace jp.lilxyzw.shadercore
         // AssetDatabaseを使わないアセットパス取得
         public static IEnumerable<string> GetFiles(string searchPattern)
         {
-            var directories = new List<string>(){"Assets/", "Packages/", "Library/PackageCache"};
-            if (File.Exists("Packages/manifest.json"))
+            var directories = new List<string>(){"Assets/"};
+            foreach (var package in PackageInfo.GetAllRegisteredPackages())
             {
-                var manifest = Newtonsoft.Json.JsonConvert.DeserializeObject<PackageManifest>(File.ReadAllText("Packages/manifest.json"));
-                var directory = "Packages";
-                directories.AddRange(manifest.dependencies.Select(kv => kv.Value).Where(p => p.StartsWith("file:")).Select(p => {
-                    try
-                    {
-                        var path = p[5..];
-                        if (Path.IsPathRooted(path)) return path;
-                        return "Packages/" + path;
-                    }
-                    catch
-                    {
-                        UnityEngine.Debug.LogError($"Invalid package path. {p}");
-                        return null;
-                    }
-                }).Where(p => !string.IsNullOrEmpty(p)));
+                if (package.source != PackageSource.BuiltIn && package.resolvedPath != null && Directory.Exists(package.resolvedPath))
+                {
+                    directories.Add(package.resolvedPath);
+                }
             }
             return directories.SelectMany(d => Directory.GetFiles(d, searchPattern, SearchOption.AllDirectories)).Select(p => p.Replace('\\', '/'));
         }
@@ -35,12 +24,6 @@ namespace jp.lilxyzw.shadercore
         public static IEnumerable<string> GetFiles(string searchPattern, string directory)
         {
             return Directory.GetFiles(directory, searchPattern, SearchOption.AllDirectories).Select(p => p.Replace('\\', '/'));
-        }
-
-        [Serializable]
-        private class PackageManifest
-        {
-            public Dictionary<string,string> dependencies = new();
         }
     }
 }
